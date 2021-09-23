5425c5fece0ee36c5b9fa2d874e2c06ec7ce5172
chedim <chedim@chedim.com>
2021-09-23 17:55:10 -0400

    go version

--- dnsconfig_androiddnsfix.go
+++ dnsconfig_androiddnsfix.go
@@ -46,7 +46,7 @@ type resolverConfig struct {
 	dnsConfig *dnsConfig   // parsed resolv.conf structure used in lookups
 }
 
-//go:linkname (*resolverConfig).tryUpdate(string) net.(*resolverConfig).tryUpdate(string)
+//go:linkname net.(*resolverConfig).tryUpdate
 func (*resolverConfig) tryUpdate(string)
 
 // Need an empty .s file (dnsconfig_empty.s)
